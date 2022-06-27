acad22271c089f6cf2ef68f497fedd72134fbd91
MajorHorse <mylove103@foxmail.com>
2022-06-27 13:06:23 +0800

    消费者 服务逻辑层编码

---
+++ majortom-cloud-consumer-order/src/main/java/com/majortom/cloud/config/ApplicationContextConfig.java
@@ -0,0 +1,22 @@
+package com.majortom.cloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * <p>
+ * RestTemplate 配置类
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/6/27 12:43
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
--- majortom-cloud-consumer-order/src/main/java/com/majortom/cloud/controller/OrderController.java
+++ majortom-cloud-consumer-order/src/main/java/com/majortom/cloud/controller/OrderController.java
@@ -1,7 +1,15 @@
 package com.majortom.cloud.controller;
 
+import com.majortom.cloud.eneity.CommonResult;
+import com.majortom.cloud.eneity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -13,5 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
+@RequestMapping(value = "/consummer")
 public class OrderController {
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> get(@PathVariable Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get" + id, CommonResult.class);
+    }
+
 }
