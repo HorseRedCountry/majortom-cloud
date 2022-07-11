1c5541c154e106fac39e3f5ed8292ae67e03814e
MajorHorse <mylove103@foxmail.com>
2022-07-11 12:46:31 +0800

    全局filter

---
+++ majortom-cloud-gatewa9527/src/main/java/com/majortom/cloud/filter/LogGateWayFilter.java
@@ -0,0 +1,41 @@
+package com.majortom.cloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * <p>
+ *
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/7/11 12:38
+ */
+@Component
+@Slf4j
+public class LogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("*************come in LogGateWayFilter： " + new Date());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (null == uname) {
+            log.info("*********用户名为null，非法用户！");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
