ef088cc90b62865bf7e77a8bcb8a2e16229c598c
MajorHorse <mylove103@foxmail.com>
2022-07-11 23:20:36 +0800

    引入Config

---
+++ majortom-cloud-config-center-3344/src/main/java/com/majortom/cloud/ConfigCenterMain3344.java
@@ -0,0 +1,21 @@
+package com.majortom.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * <p>
+ *
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/7/11 22:39
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
