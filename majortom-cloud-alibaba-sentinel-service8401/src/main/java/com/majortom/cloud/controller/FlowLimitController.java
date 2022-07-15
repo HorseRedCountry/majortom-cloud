db6b4e83a50269fc61eb16edae1d8af3a16a61ba
MajorHorse <mylove103@foxmail.com>
2022-07-15 13:01:21 +0800

    Sentinel规则测试

--- majortom-cloud-alibaba-sentinel-service8401/src/main/java/com/majortom/cloud/controller/FlowLimitController.java
+++ majortom-cloud-alibaba-sentinel-service8401/src/main/java/com/majortom/cloud/controller/FlowLimitController.java
@@ -1,8 +1,13 @@
 package com.majortom.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -25,6 +30,35 @@ public class FlowLimitController {
     public String testB() {
         log.info(Thread.currentThread().getName() + "\t" + "******testB");
         return "---------------testB";
+    }
+
+    @GetMapping(value = "/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("******testD**********");
+        return "---------------testB";
+    }
+
+    @GetMapping(value = "/testE")
+    public String testE() {
+        log.info("******testE**********");
+        int age = 10 / 0;
+        return "---------------testE";
+    }
+
+    @GetMapping(value = "/testHotkey")
+    @SentinelResource(value = "testHotkey", blockHandler = "dealTestHotkey")
+    public String testHotkey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "---------Test Hotkey";
+    }
+
+    public String dealTestHotkey(String p1, String p2, BlockException exception) {
+        return "---------dealTestHotkey";
     }
 
 }
