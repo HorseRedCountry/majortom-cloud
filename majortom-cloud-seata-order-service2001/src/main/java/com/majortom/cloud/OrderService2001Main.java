447539126d4e90df2be5d59aad370d300b307407
MajorHorse <mylove103@foxmail.com>
2022-07-25 18:26:17 +0800

    Order2001 配置修改

--- majortom-cloud-seata-order-service2001/src/main/java/com/majortom/cloud/OrderService2001Main.java
+++ majortom-cloud-seata-order-service2001/src/main/java/com/majortom/cloud/OrderService2001Main.java
@@ -3,6 +3,7 @@ package com.majortom.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <p>
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class OrderService2001Main {
     public static void main(String[] args) {
         SpringApplication.run(OrderService2001Main.class, args);
